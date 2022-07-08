58490f21f13a2d52f8cd3d07a23a32ade90aedf3
Adriana Gutierrez <102387476+DrikaDev@users.noreply.github.com>
2022-07-08 16:04:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ function verificar(){
     if(usuario.value && senha.value.length>=8){
         botaoSeta.classList.add("ativarCor")
         //console.log(botaoSeta);
+        botaoSeta.setAttribute("disabled");
     }else{
         botaoSeta.classList.remove("ativarCor");
+        botaoSeta.setAttribute("disabled", "disabled");
     }
 }
 
 usuario.addEventListener("input", verificar);
 senha.addEventListener("input", verificar);
-
